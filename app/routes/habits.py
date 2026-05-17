ce46cdf37b994ef1942aaa42a7648720ca813744
Анастасия <a8960514@gmail.com>
2026-05-17 23:10:11 +0300

    Add GET, PUT AND DELETE

--- app/routes/habits.py
+++ app/routes/habits.py
@@ -43,4 +43,37 @@ def create_habit():
 
     return jsonify(habit.to_dict()), 201
 
+@habits_bp.route("/<int:habit_id>", methods=["GET"])
+@jwt_required()
+def get_habit():
+    user = get_current_user()
+    habit = Habit.query.filter_by(id = habit.id, user_id= user.id).first_or_404()
 
+    d = habit.to_dict()
+    d["completed_today"] = habit.completed_today() 
+    d["current_streak"] = habit.current_streak()
+    return jsonify(d), 200 
+
+@habits_bp.route("/<int:habit_id>", methods=["PUT"])
+@jwt_required() 
+def update_habit():
+    user = get_current_user()
+    habit = Habit.query.filter_by(id = habit.id, user_id= user.id).first_or_404()
+    data = request.get_json() or {}
+
+    if "name" in data:
+        habit.name = data["name"].strip()
+    if "description" in data:
+        habit.description = data["description"].strip()
+
+    db.session.commit()
+    return jsonify(habit.to_dict()), 200
+
+@habits_bp.route("/<int:habit_id>", methods=["DELETE"])
+@jwt_required()
+def delete_habit(habit_id):
+    user = get_current_user()
+    habit = Habit.query.filter_by(id = habit.id, user_id= user.id).first_or_404()
+    habit.is_active = False
+    db.session.commit()
+    return jsonify({"message": f"Навикът '{habit.name}' е изтрит"}), 200
